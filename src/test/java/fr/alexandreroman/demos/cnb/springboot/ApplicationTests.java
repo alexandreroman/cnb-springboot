304d8506b32ffc7ea21f516504491420787c83c3
Alexandre Roman <aroman@pivotal.io>
2020-01-13 21:36:19 +0100

    Fix compilation errors

--- src/test/java/fr/alexandreroman/demos/cnb/springboot/ApplicationTests.java
+++ src/test/java/fr/alexandreroman/demos/cnb/springboot/ApplicationTests.java
@@ -16,18 +16,15 @@
 
 package fr.alexandreroman.demos.cnb.springboot;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ApplicationTests {
     @Autowired
