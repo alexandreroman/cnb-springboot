bf35eab359848011f2d42174abf69705f591d12b
Alexandre Roman <aroman@pivotal.io>
2020-04-27 20:22:49 +0200

    Set application name as a tag in all metrics

--- src/main/java/fr/alexandreroman/demos/cnb/springboot/MetricsConfig.java
+++ src/main/java/fr/alexandreroman/demos/cnb/springboot/MetricsConfig.java
@@ -20,6 +20,8 @@ import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Tags;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -35,6 +37,12 @@ import java.util.stream.Collectors;
 @Slf4j
 class MetricsConfig {
     private Counter appInfoCounter;
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags(
+            @Value("${spring.application.name}") String applicationName) {
+        return registry -> registry.config().commonTags("application", applicationName);
+    }
 
     @Bean
     Counter appInfo(MeterRegistry reg) {
